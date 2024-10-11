f9644c904168d68c811e569b049db70809f3dd13
nitzano707 <nitzan.orot@gmail.com>
2024-10-11 11:37:30 +0300

    Create generate.js

---
+++ functions/generate.js
@@ -0,0 +1,21 @@
+const fetch = require('node-fetch');
+
+exports.handler = async function (event) {
+    const { prompt } = JSON.parse(event.body);
+
+    const response = await fetch("https://api-inference.huggingface.co/models/gpt2", {
+        method: "POST",
+        headers: {
+            "Authorization": `Bearer ${process.env.HUGGING_FACE_API_KEY}`,
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ inputs: prompt })
+    });
+
+    const result = await response.json();
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify({ generated_text: result[0].generated_text })
+    };
+};
